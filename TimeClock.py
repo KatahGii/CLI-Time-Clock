60abaf8f1e103ec2d5c199c1d797a054aa6a7ee9
KatahGii <70392903+KatahGii@users.noreply.github.com>
2023-10-03 13:00:32 -0500

    Update TimeClock.py
    Added the ability to edit employee times manually

--- TimeClock.py
+++ TimeClock.py
@@ -1,87 +1,139 @@
-import datetime
 import os
 import pickle
+import datetime
+
+class TimeLog:
+    def __init__(self):
+        self.clock_in = datetime.datetime.now()
+        self.clock_out = None
 
 class Employee:
     def __init__(self, name, hourly_rate):
-        global next_employee_number
-        self.employee_number = next_employee_number
-        next_employee_number += 1
         self.name = name
         self.hourly_rate = hourly_rate
-        self.logged_in_time = None
-        self.total_hours = 0
+        self.time_logs = []
 
     def log_time(self):
-        if not self.logged_in_time:
-            self.logged_in_time = datetime.datetime.now()
-            print(f"{self.name} (Employee #{self.employee_number}) logged in at {self.logged_in_time}")
+        if not self.time_logs or self.time_logs[-1].clock_out:
+            self.time_logs.append(TimeLog())
+            print(f"Clocked in at {self.time_logs[-1].clock_in}")
         else:
-            logged_out_time = datetime.datetime.now()
-            hours_worked = (logged_out_time - self.logged_in_time).seconds / 3600
-            self.total_hours += hours_worked
-            print(f"{self.name} (Employee #{self.employee_number}) logged out at {logged_out_time}, worked for {hours_worked:.2f} hours.")
-            self.logged_in_time = None
+            self.time_logs[-1].clock_out = datetime.datetime.now()
+            print(f"Clocked out at {self.time_logs[-1].clock_out}")
+
+    def get_total_hours(self):
+        total_seconds = sum([(log.clock_out - log.clock_in).total_seconds() for log in self.time_logs if log.clock_out])
+        return total_seconds / 3600
 
     def get_pay(self):
-        overtime_hours = 0
-        regular_hours = self.total_hours
-        if self.total_hours > 40:
-            overtime_hours = self.total_hours - 40
-            regular_hours = 40
-        regular_pay = regular_hours * self.hourly_rate
-        overtime_pay = overtime_hours * self.hourly_rate * 1.5
-        return regular_pay + overtime_pay
+        return self.get_total_hours() * self.hourly_rate
 
-FILE_NAME = "employee_data.pkl"
-if os.path.exists(FILE_NAME):
-    with open(FILE_NAME, "rb") as file:
-        employees, next_employee_number = pickle.load(file)
-else:
-    employees = {}
-    next_employee_number = 1
+def display_edit_screen(employee):
+    while True:
+        os.system('clear')
+        print(f"Editing time logs for {employee.name}")
+
+        # Get unique dates
+        unique_dates = sorted(list(set([log.clock_in.date() for log in employee.time_logs])))
+
+        # Print headers
+        print("\nEntry No. |", " | ".join([date.strftime("%Y-%m-%d") for date in unique_dates]))
+
+        # Print times for each date
+        for i, log in enumerate(employee.time_logs, 1):
+            times_for_date = [f"{log.clock_in.time()} - {log.clock_out.time() if log.clock_out else 'Not logged out'}" if log.clock_in.date() == date else "" for date in unique_dates]
+            print(f"{i}.       |", " | ".join(times_for_date))
+
+        try:
+            entry_num = int(input("\nEnter the number of the entry you want to edit (0 to go back): "))
+
+            if entry_num == 0:
+                break
+            elif 0 < entry_num <= len(employee.time_logs):
+                selected_log = employee.time_logs[entry_num - 1]
+
+                # Editing only times, not dates
+                new_clock_in_time = input(f"Enter new Clock In TIME for entry {entry_num} (HH:MM:SS or leave empty for no change): ")
+                new_clock_out_time = input(f"Enter new Clock Out TIME for entry {entry_num} (HH:MM:SS or 'none' for not logged out, or leave empty for no change): ")
+
+                if new_clock_in_time:
+                    time_parts = [int(part) for part in new_clock_in_time.split(':')]
+                    selected_log.clock_in = selected_log.clock_in.replace(hour=time_parts[0], minute=time_parts[1], second=time_parts[2])
+
+                if new_clock_out_time == 'none':
+                    selected_log.clock_out = None
+                elif new_clock_out_time:
+                    time_parts = [int(part) for part in new_clock_out_time.split(':')]
+                    selected_log.clock_out = selected_log.clock_in.replace(hour=time_parts[0], minute=time_parts[1], second=time_parts[2])
+
+        except ValueError:
+            print("Please enter a valid number.")
+            input("\nPress enter to try again...")
+
 
 def save_data():
-    with open(FILE_NAME, "wb") as file:
+    with open('employee_data.pkl', 'wb') as file:
         pickle.dump((employees, next_employee_number), file)
 
-def menu():
-    os.system('clear')  # This is for Unix-based systems. Use os.system('cls') for Windows.
-    print("1. Add Employee")
-    print("2. Clock In/Out")
-    print("3. View Payroll")
-    print("4. Exit")
+def load_data():
+    try:
+        with open('employee_data.pkl', 'rb') as file:
+            return pickle.load(file)
+    except (FileNotFoundError, EOFError):
+        return {}, 1
 
-while True:
-    menu()
-    choice = input("Enter your choice: ")
-    
-    if choice == "1":
-        name = input("Enter employee name: ")
-        hourly_rate = float(input("Enter hourly rate: "))
-        employee = Employee(name, hourly_rate)
-        employees[employee.employee_number] = employee
-        print(f"Employee {name} added with Employee Number: {employee.employee_number}")
-        save_data()
+def main_menu():
+    global employees
+    global next_employee_number
 
-    elif choice == "2":
-        try:
-            emp_num = int(input("Enter employee number: "))
+    while True:
+        os.system('clear')
+        print("Time Clock System")
+        print("1. Clock In/Out")
+        print("2. Add Employee")
+        print("3. View Payroll")
+        print("4. Edit Clock In/Out Times")
+        print("5. Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            emp_num = int(input("Enter your employee number: "))
             if emp_num in employees:
                 employees[emp_num].log_time()
+            else:
+                print("Employee not found!")
+            save_data()
+
+        elif choice == "2":
+            name = input("Enter employee name: ")
+            hourly_rate = float(input("Enter hourly rate: "))
+            employees[next_employee_number] = Employee(name, hourly_rate)
+            print(f"Employee added with number: {next_employee_number}")
+            next_employee_number += 1
+            save_data()
+
+        elif choice == "3":
+            for emp_num, employee in employees.items():
+                total_hours = employee.get_total_hours()
+                pay_due = employee.get_pay()
+                print(f"Employee {employee.name} ({emp_num}): {total_hours:.2f} hours worked. Pay due: ${pay_due:.2f}")
+
+        elif choice == "4":
+            emp_num = int(input("Enter employee number to edit times: "))
+            if emp_num in employees:
+                display_edit_screen(employees[emp_num])
                 save_data()
             else:
                 print("Employee not found!")
-        except ValueError:
-            print("Please enter a valid employee number!")
 
-    elif choice == "3":
-        for emp_num, employee in employees.items():
-            print(f"{employee.name} (Employee #{emp_num}) has worked {employee.total_hours:.2f} hours and is owed ${employee.get_pay():.2f}.")
+        elif choice == "5":
+            break
 
-    elif choice == "4":
-        break
-    else:
-        print("Invalid choice!")
+        else:
+            print("Invalid choice!")
 
-    input("\nPress enter to continue...")
+        input("\nPress enter to continue...")
+
+if __name__ == "__main__":
+    employees, next_employee_number = load_data()
+    main_menu()
